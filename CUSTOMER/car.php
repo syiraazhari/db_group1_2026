5c8e0e022693e7c02a70d87ee50ceecab8accab4
7wsy <oneamsyar05@gmail.com>
2026-06-11 00:27:50 +0800

    UPDATE EMAS7

--- CUSTOMER/car.php
+++ CUSTOMER/car.php
@@ -68,13 +68,13 @@
     		<div class="row">
     			<div class="col-md-4">
     				<div class="car-wrap rounded ftco-animate">
-    					<div class="img rounded d-flex align-items-end" style="background-image: url(images/car-1.jpg);">
+    					<div class="img rounded d-flex align-items-end" style="background-image: url(images/aruz.jpg);">
     					</div>
     					<div class="text">
-    						<h2 class="mb-0"><a href="car-single.php">Mercedes Grand Sedan</a></h2>
+    						<h2 class="mb-0"><a href="car-single.php">Perodua Aruz</a></h2>
     						<div class="d-flex mb-3">
 	    						<span class="cat">Cheverolet</span>
-	    						<p class="price ml-auto">$500 <span>/day</span></p>
+	    						<p class="price ml-auto">RM65 <span>/day</span></p>
     						</div>
     						<p class="d-flex mb-0 d-block"><a href="#" class="btn btn-primary py-2 mr-1">Book now</a> <a href="car-single.php" class="btn btn-secondary py-2 ml-1">Details</a></p>
     					</div>
