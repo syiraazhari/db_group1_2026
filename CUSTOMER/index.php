39dfc72d13c84981c9f641d4c7ac42f7b44a2569
fawwaz000 <tuanfawwazsyahmi@graduate.utm.my>
2026-06-10 23:55:59 +0800

    UPDATE GAMBAR KERETA

--- CUSTOMER/index.php
+++ CUSTOMER/index.php
@@ -57,7 +57,7 @@ if (!isset($_SESSION['customer_id'])) {
         </div>
       </div>
     </nav>
-    <div class="hero-wrap ftco-degree-bg" style="background-image: url('images/bg_1.jpg');" data-stellar-background-ratio="0.5">
+    <div class="hero-wrap ftco-degree-bg" style="background-image: url('images/Bufori.jpg');" data-stellar-background-ratio="0.5">
       <div class="overlay"></div>
       <div class="container">
         <div class="row no-gutters slider-text justify-content-start align-items-center justify-content-center">
